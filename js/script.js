0ed36456379ef96ad88ca07a5627882cbc565e73
cyber-talhaaa <talhadar13579@gmail.com>
2021-08-31 10:12:26 +0500

    Responsivenesss Added

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
 //*********************************************************** */
+//* NAVIGATION TOGGLE
+const navMenu = document.querySelector('.main__nav--menu');
+const toggleBtn = document.querySelector('.main__nav--toggle');
+
+toggleBtn.addEventListener('click', (e) => {
+  navMenu.classList.toggle('display-flex');
+  toggleBtn.classList.toggle('toggle--nav');
+});
+
+//*********************************************************** */
 //* VIDEO PLAY
 const playBtn = document.querySelector('.video__content--play__button');
 const video = document.querySelector('.video__content--video');
 
-playBtn.addEventListener('click', function() {
-    if (video.paused) {
-        video.play();
-    }
-    playBtn.style.display = 'none';
-    video.setAttribute('controls', 'controls');
-    video.setAttribute('muted', 'muted');
-})
+playBtn.addEventListener('click', () => {
+  if (video.paused) video.play();
+
+  playBtn.style.display = 'none';
+  video.setAttribute('controls', 'controls');
+  video.setAttribute('muted', 'muted');
+});
